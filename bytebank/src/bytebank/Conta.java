fd7cc257ad89447d49746951215b91041454d8e6
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-11-27 23:57:36 -0300

    transferencia

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -6,7 +6,26 @@ public class Conta {
 	int numero;
 	String titular;
 	
-	void deposita(double valor) {
+	public void deposita(double valor) {
 		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -3,11 +3,24 @@ package bytebank;
 public class TestaMetodo {
 	
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(50);
+		Conta conta1 = new Conta();
+		conta1.saldo = 100;
+		conta1.deposita(50);
+		System.out.println(conta1.saldo);
 		
-		System.out.println(conta.saldo);
-	}
-	
+		boolean conseguiuRetirar = conta1.saca(20);
+		System.out.println(conta1.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta conta2 = new Conta();
+		conta2.deposita(1000);
+		
+		if(conta2.transfere(300, conta1)) {
+			System.out.println("transferência feita com sucesso");
+		} else {
+			System.out.println("transferência não realizada");
+		}
+		System.out.println(conta1.saldo);
+		System.out.println(conta2.saldo);
+	}	
 }
