49ec9dc3a5ffa7182503c2d4f83bd766ba5e9ee8
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-12-10 23:50:29 -0300

    funcionario

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		 
+		Funcionario funcHenrique = new Funcionario();
+		funcHenrique.setNome("Henrique Fittipaldi");
+		funcHenrique.setCpf("00000000000");
+		funcHenrique.setSalario(3000.00);
+		
+		
+		
+	}
+	
+}
